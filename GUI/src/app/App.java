69ba3ff7f2e15d2efed8e9b961eb9d63ab590c7d
erthang <endrewrthang@gmail.com>
2019-10-23 16:59:00 -0300

    ui 2

--- GUI/src/app/App.java
+++ GUI/src/app/App.java
@@ -1,6 +1,6 @@
 package app;
 
-import java.awt.Container;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -15,15 +15,14 @@ import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 
 public class App extends JFrame {
+    private static final long serialVersionUID = -8412987007488059947L;
     JTextField entrada;
     JToggleButton leguas, milhas, milhasMaritimas, KM, metro, jardas, pes, polegadas, milimetros;
-    JLabel saida;
+    JLabel saida, textoEntrada;
     JPanel painel, painelEntrada, painelSaida, painelDados;
     ButtonGroup grupo;
 
     public App() {
-
-        Container c = getContentPane();
 
         grupo = new ButtonGroup();
 
@@ -33,8 +32,14 @@ public class App extends JFrame {
 
         painelDados = new JPanel(new GridLayout(3, 3));
 
+        //textoEntrada
+        textoEntrada = new JLabel();
+        textoEntrada.setText("Valor: ");
+        textoEntrada.setFont(new Font("Verdana", 1, 50));
+        painelEntrada.add(textoEntrada, BorderLayout.EAST);
+
         // Entrada
-        entrada = new JTextField("Valor em metros");
+        entrada = new JTextField();
         entrada.setPreferredSize(new Dimension(600, 100));
         entrada.setFont(new Font("Arial", Font.BOLD, 50));
         painelEntrada.add(entrada);
---
+++ GUI2/src/app/App.java
@@ -0,0 +1,195 @@
+package app;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+
+public class App extends JFrame {
+    private static final long serialVersionUID = 5430513214412853815L;
+    JPanel painel, painelBotoes, painelSaida, painelUm, painelDois;
+    JTextField valorUm, valorDois;
+    JToggleButton botaoSoma, botaoMenos, botaoDiv, botaoMul;
+    JButton botaoIgual;
+    JLabel textUm, textDois, saida, textSaida;
+    ButtonGroup grupo;
+
+    public App() {
+        painel = new JPanel(new GridLayout(4, 1));
+        painelDois = new JPanel();
+        painelUm = new JPanel();
+        painelBotoes = new JPanel(new GridLayout(2, 2));
+        painelSaida = new JPanel(new GridLayout(2, 1));
+        saida = new JLabel();
+
+        // texto valor um
+        textUm = new JLabel();
+        textUm.setText("Valor: ");
+        textUm.setFont(new Font("Arial", 1, 50));
+        painelUm.add(textUm, "North");
+
+        valorUm = new JTextField();
+        valorUm.setPreferredSize(new Dimension(600, 80));
+        valorUm.setFont(new Font("Arial", 1, 50));
+        painelUm.add(valorUm);
+
+        // texto valor dois
+        textDois = new JLabel();
+        textDois.setText("Valor: ");
+        textDois.setFont(new Font("Arial", 1, 50));
+        painelDois.add(textDois);
+
+        valorDois = new JTextField();
+        valorDois.setPreferredSize(new Dimension(600, 80));
+        valorDois.setFont(new Font("Arial", 1, 50));
+        painelDois.add(valorDois);
+
+        // botoes
+        // +
+        botaoSoma = new JToggleButton("+");
+        botaoSoma.setPreferredSize(new Dimension(200, 100));
+        botaoSoma.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    float v1 = Float.parseFloat(valorUm.getText()); 
+                    float v2 = Float.parseFloat(valorDois.getText());
+                    String res = Float.toString(v1 + v2);
+                    saida.setText(res);
+                }catch(Exception e2){
+                    JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        painelBotoes.add(botaoSoma);
+        grupo.add(botaoSoma);
+
+        // -
+        botaoMenos = new JToggleButton("-");
+        botaoMenos.setPreferredSize(new Dimension(200, 100));
+        botaoMenos.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                this.op = 0;
+            }
+        });
+        grupo.add(botaoMenos);
+        painelBotoes.add(botaoMenos);
+
+        // /
+        botaoDiv = new JToggleButton("/");
+        botaoDiv.setPreferredSize(new Dimension(200, 100));
+        botaoDiv.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+        grupo.add(botaoDiv);
+        painelBotoes.add(botaoDiv);
+
+        // x
+        botaoMul = new JToggleButton("x");
+        botaoMul.setPreferredSize(new Dimension(200, 100));
+        botaoMul.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+        grupo.add(botaoMul);
+        painelBotoes.add(botaoMul);
+
+        // = 
+        botaoIgual = new JButton("=");
+        botaoIgual.setPreferredSize(new Dimension(200, 100));
+        botaoIgual.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch(this.op){
+                    case 0:
+                        try{
+                            float v1 = Float.parseFloat(valorUm.getText()); 
+                            float v2 = Float.parseFloat(valorDois.getText());
+                            String res = Float.toString(v1 - v2);
+                            saida.setText(res);
+                        }catch(Exception e2){
+                            JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                        }
+                    case 1:
+                        try{
+                            float v1 = Float.parseFloat(valorUm.getText()); 
+                            float v2 = Float.parseFloat(valorDois.getText());
+                            if(v2 == 0){
+                                JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                                return;
+                            }
+                            String res = Float.toString(v1 / v2);
+                            saida.setText(res);
+                        }catch(Exception e2){
+                            JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                        }
+                    case 2:
+                        try{
+                            float v1 = Float.parseFloat(valorUm.getText()); 
+                            float v2 = Float.parseFloat(valorDois.getText());
+                            if(v2 == 0){
+                                JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                                return;
+                            }
+                            String res = Float.toString(v1 / v2);
+                            saida.setText(res);
+                        }catch(Exception e2){
+                            JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                        }
+                    case 3:
+                        try{
+                            float v1 = Float.parseFloat(valorUm.getText()); 
+                            float v2 = Float.parseFloat(valorDois.getText());
+                            String res = Float.toString(v1 * v2);
+                            saida.setText(res);
+                        }catch(Exception e2){
+                            JOptionPane.showMessageDialog(null, "Entrada Inválida", "ERRO", JOptionPane.ERROR_MESSAGE);
+                        }
+                }
+            }
+
+        });
+        painelBotoes.add(botaoIgual);
+
+
+        //resultado
+        textSaida = new JLabel();
+        textSaida.setText("Resultado: ");
+        textSaida.setFont(new Font("Arial", 1, 50));
+        painelSaida.add(textSaida);
+
+        saida.setFont(new Font("Arial", 1, 50));
+        saida.setPreferredSize(new Dimension(600, 80));
+        painelSaida.add(saida);
+        
+
+        painel.add(painelUm);
+        painel.add(painelDois);
+        painel.add(painelBotoes);
+        painel.add(painelSaida);
+
+        setSize(800, 600);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+        setContentPane(painel);
+    }
+}
